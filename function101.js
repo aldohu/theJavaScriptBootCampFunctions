234e9bb22cd60ab0592e05f94bba3a819f94cc2f
Aldin <alhuric89@gmail.com>
2021-10-28 22:35:56 +0200

    add fahrenit funciton

--- function101.js
+++ function101.js
@@ -1,1 +1,19 @@
-console.log("Hello World ")
+let greetUser = function () {
+    console.log("Hello")
+}
+
+
+greetUser();
+
+let square = function(num) {
+    console.log(num * num);
+}
+square(3);
+
+
+const convertFahrenheitToCelsius = function(f){
+    return (f - 32) * 1.8;
+}
+
+console.log(convertFahrenheitToCelsius(32));
+console.log(convertFahrenheitToCelsius(68))
